0b73c73b6c91b6e224689d00d91312537ece048a
SueYeong <dudtb205@gmail.com>
2022-06-08 10:41:19 +0900

    setTimeout, setInterval

--- js/timeFn.js
+++ js/timeFn.js
@@ -1,5 +1,31 @@
 (function(){
 
-    
+    // *setTimeout
+    // =>지정한 시간 내에 한번만 실행함
+    // =>setTimeout(() => {
+    //     실행식
+    // }, 시간);
 
-})()
+    // *setInterval
+    // =>지정한 시간 마다 계속 실행함
+    // =>setInterval(() => {
+    //     실행식
+    // }, 시간);
+
+    // setTimeout(() => {
+    //     document.write('2초뒤에 한번만 실행함')
+    // }, 2000)
+
+    // const handleTime = () => {
+    //     document.write('한번만 실행 짠!')
+    // }
+
+    // setTimeout(handleTime, 2000)
+
+    // setInterval(() => {
+    //     document.write('2초마다 계속 실행됨')
+    // }, 2000)
+
+
+
+})();
